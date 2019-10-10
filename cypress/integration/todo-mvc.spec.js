017620f504b12222e6b13dec3e7e163a36f09086
Misaelreis <zael.au@gmail.com>
2019-10-09 23:07:00 -0300

    Update

--- cypress/integration/todo-mvc.spec.js
+++ cypress/integration/todo-mvc.spec.js
@@ -15,36 +15,48 @@ import {
 
 import { createTodo } from "../factory/todo-mvc.factory";
 
-
 describe('testes e2e lista de tarefas', ()=>{
     beforeEach(()=>{
         visit()
+        addTodo(createTodo())
     })
     it('validar placeholder', ()=>{
         checkPlaceHolder('What needs to be done?')
     })
 
     it('validar se item da lista ta marcado', ()=>{
-        addTodo('Arrumar Quarto')
         completeTodo(1)
         checked(1)
     })
 
     it('validar exclusão de item lista', ()=>{
-        addTodo('Arrumar Quarto')
         destroyTodo(1)
         emptyTodoList()
     })
 
-    it('validar nome da tarefa na lista', ()=>{
-        addTodo('Arrumar Quarto')
-        checkLabelText(1,'Arrumar Quarto')
-    })
-
     it('validar texto cortado após selecionar tarefa', ()=>{
-        addTodo('Arrumar Quarto')
         completeTodo(1)
         textDecoration(1)
+    })
+
+    it('validar lista vazia', ()=>{
+        completeTodo(1)
+        clearCompleted()
+        emptyTodoList()
+    })
+
+   it('gerar lista aleatória', ()=>{
+    var i;
+    for (i= 0; i < 10; i++) {
+        addTodo(createTodo())
+    }
+    })
+
+    /* Testes a serem alterados utilizando actions.
+
+       it('validar nome da tarefa na lista', ()=>{
+        addTodo('Arrumar Quarto')
+        checkLabelText(1,'Arrumar Quarto')
     })
 
     it('validar quantidade de items restantes', ()=>{
@@ -59,37 +71,7 @@ describe('testes e2e lista de tarefas', ()=>{
         completeTodo(2)
         validateItemsLeft(1)
     })
-
-    it('limpar itens completados', ()=>{
-        addTodo('Arrumar Quarto')
-        addTodo('Lavar louça')
-        validateItemsLeft(2)
-        completeTodo(1)
-        checkLabelText(1,'Arrumar Quarto')
-        clearCompleted()
-        validateItemsLeft(1)
-        checkLabelText(1,'Lavar louça')
-    })
-
-    it('validar lista vazia', ()=>{
-        addTodo('Arrumar Quarto')
-        completeTodo(1)
-        clearCompleted()
-        emptyTodoList()
-    })
-
-    it('gerar lista aleatória', ()=>{
-        addTodo(createTodo())
-        addTodo(createTodo())
-        addTodo(createTodo())
-        addTodo(createTodo())
-        addTodo(createTodo())
-        addTodo(createTodo())
-        addTodo(createTodo())
-        addTodo(createTodo())
-        addTodo(createTodo())
-        addTodo(createTodo())
-        addTodo(createTodo())
-        addTodo(createTodo())
-    })
+     */
 })
+
+
